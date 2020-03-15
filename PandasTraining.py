82e40b1cce5a24a1c915c55bcea6c4c649d3bfc3
Ivan Borodin <54885403+PolyrniyMak@users.noreply.github.com>
2020-03-15 16:53:44 +0300

    PandasTraining

---
+++ PandasTraining.py
@@ -0,0 +1,44 @@
+ # import pandas as pd
+ # import numpy as np
+#
+# data = pd.DataFrame ({
+#     'Days': ['Monday', 'Tuesday', 'Wednesday', 'Tursday', 'Friday'],
+#     'Months' : ['Jan', 'Feb', 'Mar', 'May', np.nan]
+# })
+#
+# files = pd.read_html('https://accessibility.psu.edu/tableshtml/')
+# print(files)
+
+
+
+
+# import pandas as pd
+# df = pd.DataFrame({'id':range(15), 'chars':['ab']*15})
+# df = pd.read_excel('CsvTest.xlsx', header=0, sep='\t')
+# print(df)
+
+
+
+
+# import pandas as pd
+# df = pd.DataFrame(
+#     {'countries':['USA', 'Russia', 'China', 'Brasil'],
+#     'square':['524324', '15134513', '4567', '2523'],
+#     'population':['12000', '4000', '6000', '50000']})
+# print(df.countries)
+
+
+
+
+
+# import pandas as pd
+# df = pd.DataFrame(
+#     {'countries':['USA', 'Russia', 'China', 'Brasil'],
+#     'square':['524324', '15134513', '4567', '2523'],
+#     'population':['12000', '4000', '6000', '50000']})
+# df.index = ['U', 'R', 'Ch', 'Br']
+# print(df.loc['U'])
+
+
+
+
