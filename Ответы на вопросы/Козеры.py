bd230de1b3a2cff58dfc3b249363fe9e17e9363f
Ivan Borodin <54885403+PolyrniyMak@users.noreply.github.com>
2020-04-22 21:37:32 +0300

    Update Козеры.py

--- Ответы на вопросы/Козеры.py
+++ Ответы на вопросы/Козеры.py
@@ -1,6 +1,7 @@
 json = {"bomonque": [{"iu": [{"iu-1": []}, {"iu10": [{"cozers": ["Zversky", "GoldGem", "Cnya176"]}]}]},
                      {"fn": [{"fn-12": [{"obercozers": [], "cozers": ["Ivanov", "Petrov"]}]}]}]}
 
+a = []
 for bomonque in json:
     for group in json[bomonque]:
         for iu in group:
@@ -8,5 +9,6 @@ for bomonque in json:
                 for kaf2 in kaf:
                     for cors in kaf[kaf2]:
                         for cozerog in cors:
-                            for people in cors[cozerog]:
-                                print(people)
+                            a.append(cors[cozerog])
+                            break
+print(' '.join([str(i) for i in a[0]]))
