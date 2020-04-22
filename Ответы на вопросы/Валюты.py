1196fc4277f96d2aea156c2bc2f03a85e1cea6fa
Ivan Borodin <54885403+PolyrniyMak@users.noreply.github.com>
2020-04-22 21:18:29 +0300

    Выводит курс вместе с запятой

--- Ответы на вопросы/Валюты.py
+++ Ответы на вопросы/Валюты.py
@@ -1,3 +1,4 @@
 import pandas as pd
-df = pd.read_html('https://www.cbr.ru/currency_base/daily/', header=0)
+df = pd.read_html('https://www.cbr.ru/currency_base/daily/', header=0, parse_dates=['Курс'])
 print(df)
+
